ee2d1b47b3557c9faf62ce3be7c0a7b90df63695
Yuki Nishijima <mail@yukinishijima.net>
2014-02-22 18:34:55 -0800

    Get copiling working when it is installed through bundler

--- ext/did_you_mean/extconf.rb
+++ ext/did_you_mean/extconf.rb
@@ -1,15 +1,13 @@
 require 'mkmf'
 
-$CFLAGS += " -O0"
-$CFLAGS += " -std=c99"
-
 case RUBY_VERSION
-when /1.9.3/
-  $CFLAGS += " -I./ruby_headers/193"
-when /2.0.0/
-  $CFLAGS += " -I./ruby_headers/200"
-when /2.1.0/
-  $CFLAGS += " -I./ruby_headers/210"
+when /1.9.3/, /2.0.0/, /2.1.0/
+  abs_ruby_header_path = File.join(File.dirname(File.realpath(__FILE__)), "ruby_headers")
+  version_suffix       = RUBY_VERSION.tr(".", "")
+
+  $CFLAGS += " -I#{abs_ruby_header_path}/#{version_suffix}"
+else
+  raise "Sorry, but did_you_mean #{DidYouMean::VERSION} doesn't work with Ruby #{RUBY_VERSION}."
 end
 
 create_makefile 'method_missing'

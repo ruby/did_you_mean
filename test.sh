4dd603f908959090becb4cf5a141bfbb03dd6aaa
Yuki Nishijima <mail@yukinishijima.net>
2014-04-19 17:04:46 -0700

    Better output for test script

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 for ruby_version in 1.9.3-p545 2.0.0-p451 2.1.0 2.1.1 head
 do
-  echo "Testing did_you_mean for Ruby $ruby_version"
+  echo -e "\e[32mTesting did_you_mean for Ruby $ruby_version\e[39m"
 
   source ~/.rvm/environments/ruby-$ruby_version
   ruby -v
@@ -13,4 +13,6 @@ do
   rake compile
   appraisal rake test:without_compile
   rake clobber
+
+  echo ""
 done

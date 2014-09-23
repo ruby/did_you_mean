edd1016efd3f2401f9d45cfc3655b9090de9a97d
Yuki Nishijima <mail@yukinishijima.net>
2014-09-22 18:08:33 -0700

    Remove text gem and put our own Levenshtein implementation

--- did_you_mean.gemspec
+++ did_you_mean.gemspec
@@ -19,7 +19,6 @@ Gem::Specification.new do |spec|
   spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
   spec.require_paths = ["lib"]
 
-  spec.add_dependency "text"
   spec.add_dependency "binding_of_caller"
 
   spec.add_development_dependency "bundler", "~> 1.5"
--- lib/did_you_mean/method_finder.rb
+++ lib/did_you_mean/method_finder.rb
@@ -1,5 +1,3 @@
-require "text"
-
 module DidYouMean
   class MethodMatcher
     attr_reader :method_collection, :target_method
@@ -11,7 +9,7 @@ module DidYouMean
 
     def similar_methods
       @similar_methods ||= method_collection.select do |method|
-        ::Text::Levenshtein.distance(method.to_s, target_method) <= threshold
+        Levenshtein.distance(method.to_s, target_method) <= threshold
       end
     end
 
@@ -20,5 +18,50 @@ module DidYouMean
     def threshold
       (target_method.size * 0.3).ceil
     end
+  end
+
+  module Levenshtein
+    # This code is based directly on the Text gem implementation
+    # Returns a value representing the "cost" of transforming str1 into str2
+    def distance(str1, str2)
+      n = str1.length
+      m = str2.length
+      return m if n.zero?
+      return n if m.zero?
+
+      d = (0..m).to_a
+      x = nil
+
+      str1.each_char.each_with_index do |char1, i|
+        e = i + 1
+        str2.each_char.each_with_index do |char2, j|
+          cost = (char1 == char2) ? 0 : 1
+          x = min3(
+            d[j+1] + 1, # insertion
+            e + 1,      # deletion
+            d[j] + cost # substitution
+          )
+          d[j] = e
+          e = x
+        end
+        d[m] = x
+      end
+
+      x
+    end
+    module_function :distance
+
+    private
+
+    def min3(a, b, c) # :nodoc:
+      if a < b && a < c
+        a
+      elsif b < a && b < c
+        b
+      else
+        c
+      end
+    end
+    module_function :min3
   end
 end

1b9a1522c2b1d8447f8bfaa124f12a889a1032ce
Yuki Nishijima <mail@yukinishijima.net>
2014-09-30 20:40:43 -0700

    Do not override frame_binding ivar in listener if already defined

--- lib/did_you_mean.rb
+++ lib/did_you_mean.rb
@@ -7,7 +7,10 @@ require "did_you_mean/finders"
 
 module DidYouMean
   Interception.listen(->(exception, binding) {
-    if exception.is_a?(NameError)
+    # On IRB/pry console, this event is called twice. In the second event,
+    # we get IRB/pry binding. So it shouldn't override @frame_binding if
+    # it's already defined.
+    if exception.is_a?(NameError) && !exception.instance_variable_defined?(:@frame_binding)
       exception.instance_variable_set(:@frame_binding, binding)
     end
   })

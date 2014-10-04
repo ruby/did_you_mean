7a9aa71bdbeec61f01c5bc43cf5b3a1eaa222735
Yuki Nishijima <mail@yukinishijima.net>
2014-10-04 10:03:40 -0700

    :scissors: DEPRECATION WARNING
    "Passing a string to AR::Base.establish_connection for a configuration lookup is deprecated, please pass a symbol (:test) instead."

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,7 +13,7 @@ begin
 
   # database
   ActiveRecord::Base.configurations = {'test' => {adapter: 'sqlite3', database: ':memory:'}}
-  ActiveRecord::Base.establish_connection('test')
+  ActiveRecord::Base.establish_connection(:test)
 
   # models
   class User < ActiveRecord::Base; end

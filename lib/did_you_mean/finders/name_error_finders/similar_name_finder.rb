39d1e2bd66d6ff8acbc4dd5da922fc7e5fcefb20
Yuki Nishijima <mail@yukinishijima.net>
2014-10-27 17:17:41 -0700

    Suggest instance variable names in case "@" is missing
    closes #12, but class/module variable name suggestion should also be
    implemented.

--- lib/did_you_mean/finders/name_error_finders/similar_name_finder.rb
+++ lib/did_you_mean/finders/name_error_finders/similar_name_finder.rb
@@ -1,16 +1,19 @@
 module DidYouMean
   class SimilarNameFinder
     include BaseFinder
-    attr_reader :name, :_methods, :_local_variables
+    attr_reader :name, :_methods, :_local_variables, :_instance_variables
 
     def initialize(exception)
       @name             = exception.name
       @_methods         = exception.frame_binding.eval("methods")
       @_local_variables = exception.frame_binding.eval("local_variables")
+      @_instance_variables = exception.frame_binding.eval("instance_variables").map do |name|
+        name.to_s.tr("@", "")
+      end
     end
 
     def words
-      local_variable_names + method_names
+      local_variable_names + method_names + instance_variable_names
     end
 
     alias target_word name
@@ -31,6 +34,14 @@ module DidYouMean
       similar_words.select{|word| word.is_a?(MethodName) }
     end
 
+    def instance_variable_names
+      _instance_variables.map {|word| InstanceVariableName.new(word.to_s) }
+    end
+
+    def similar_instance_variables
+      similar_words.select {|word| word.is_a?(InstanceVariableName) }
+    end
+
     def format(word)
       "#{word.prefix}#{word}"
     end
@@ -41,6 +52,10 @@ module DidYouMean
 
     class LocalVariableName < String
       def prefix; ""; end
+    end
+
+    class InstanceVariableName < String
+      def prefix; "@"; end
     end
   end
 end

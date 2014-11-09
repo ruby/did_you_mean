1e44703d913ca17fbf1e7460f3ee520cbd6ca830
Yuki Nishijima <mail@yukinishijima.net>
2014-11-08 16:40:59 -0800

    Make TestHelper better
      * Remove #assert_suggestions that is not used
      * Change #assert to #assert_equal for better error message/simpler code

--- lib/did_you_mean/test_helper.rb
+++ lib/did_you_mean/test_helper.rb
@@ -1,11 +1,7 @@
 module DidYouMean
   module TestHelper
     def assert_suggestion(array, expected)
-      assert (array.include?(expected) && array.one?), "Expected #{array.inspect} to only include #{expected.inspect}."
-    end
-
-    def assert_suggestions(array, *expected)
-      expected.each {|s| assert_suggestion(array, s) }
+      assert_equal [expected], array, "Expected #{array.inspect} to only include #{expected.inspect}"
     end
 
     def assert_nothing_raised

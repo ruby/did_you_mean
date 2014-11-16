94b9ce883e1a4cf688bf3b106768ac6815c31dd7
Yuki Nishijima <mail@yukinishijima.net>
2014-11-16 09:20:34 -0800

    Tell the user why compilation fails and encourage to upgrade gem
    [ci skip]

--- ext/did_you_mean/extconf.rb
+++ ext/did_you_mean/extconf.rb
@@ -1,11 +1,23 @@
 require 'mkmf'
+require_relative '../../lib/did_you_mean/version'
 
 version =
   case RUBY_VERSION
   when /1.9.3/, /2.0.0/, /2.1.[0-5]/, /2.2.\d/
     RUBY_VERSION
   else
-    raise "Sorry, but did_you_mean #{DidYouMean::VERSION} doesn't work with Ruby #{RUBY_VERSION}."
+    message =
+      "\033[91m" "Unsupported Ruby version!" "\033[39m" "\n" \
+      "\n" \
+      "\033[32m" \
+      "  Sorry, did_you_mean #{DidYouMean::VERSION} doesn't work with Ruby #{RUBY_VERSION}.\n" \
+      "  If you've upgraded Ruby recently, try upgrading did_you_mean gem:\n" \
+      "\n" \
+      "\033[0m" \
+      "      $ bundle update did_you_mean\n" \
+      "\033[39m" \
+
+    raise RuntimeError.new(message)
   end.tr(".", "")
 
 ruby_header_path = File.join(File.dirname(File.realpath(__FILE__)), "ruby_headers")

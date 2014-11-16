c53436abc050b8535284bd044693307a89734257
Yuki Nishijima <mail@yukinishijima.net>
2014-11-16 07:09:49 -0800

    Force to upgrade this gem to get it working with new Ruby versions
    To avoid introducing a bug like https://github.com/yuki24/did_you_mean/issues/14,
    did_you_mean shouldn't be able to compile without the latest ruby header
    files if a new Ruby version is released.

--- ext/did_you_mean/extconf.rb
+++ ext/did_you_mean/extconf.rb
@@ -1,18 +1,9 @@
 require 'mkmf'
 
-LATEST_RUBY_VERSION = "2.1.4"
-RUBY_TRUNK_VERSION  = "2.2.0"
-
 version =
   case RUBY_VERSION
-  when /1.9.3/, /2.0.0/
+  when /1.9.3/, /2.0.0/, /2.1.[0-5]/, /2.2.\d/
     RUBY_VERSION
-  when /^2.1.\d/, /^2.2.\d/
-    if LATEST_RUBY_VERSION < RUBY_VERSION && RUBY_VERSION < RUBY_TRUNK_VERSION
-      LATEST_RUBY_VERSION
-    else
-      RUBY_VERSION
-    end
   else
     raise "Sorry, but did_you_mean #{DidYouMean::VERSION} doesn't work with Ruby #{RUBY_VERSION}."
   end.tr(".", "")

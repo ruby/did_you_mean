23435d371bc33a35cb4183426f8140c8dc324e1d
Thomas Leishman <thomas.leishman@plansource.com>
2015-01-09 15:24:03 -0700

    NoMethodError fix for Similar ClassNames and MethodNames

--- lib/did_you_mean/finders/name_error_finders/similar_class_finder.rb
+++ lib/did_you_mean/finders/name_error_finders/similar_class_finder.rb
@@ -5,6 +5,7 @@ module DidYouMean
 
     def initialize(exception)
       @class_name, @original_message = exception.name, exception.original_message
+      autoload_class_name_inflector
     end
 
     def words
@@ -31,6 +32,13 @@ module DidYouMean
     end
 
     private
+
+    def autoload_class_name_inflector
+      return unless defined?(Rails)
+      name_is_singular = name_from_message.to_s.pluralize.singularize == name_from_message.to_s
+      class_name_inflector = name_is_singular ? name_from_message.to_s.pluralize : name_from_message.to_s.singularize
+      class_name_inflector.safe_constantize
+    end
 
     def scope_base
       @scope_base ||= (/(([A-Z]\w*::)*)([A-Z]\w*)$/ =~ original_message ? $1 : "").split("::")

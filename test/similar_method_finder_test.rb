d2b1526b538fd85282b72baa57eb6fbb21b48244
Thomas Leishman <thomas.leishman@plansource.com>
2015-01-09 11:33:01 -0700

    NoMethodError fix for Similar ClassNames and Class MethodNames #25

--- test/similar_method_finder_test.rb
+++ test/similar_method_finder_test.rb
@@ -1,23 +1,6 @@
 require_relative 'test_helper'
 
 class SimilarMethodFinderTest < Minitest::Test
-  class User
-    def friends; end
-    def first_name; end
-    def descendants; end
-
-    private
-
-    def friend; end
-
-    class << self
-      def load; end
-    end
-  end
-
-  module UserModule
-    def from_module; end
-  end
 
   def setup
     user = User.new.extend(UserModule)
@@ -26,6 +9,8 @@ class SimilarMethodFinderTest < Minitest::Test
     @error_from_private_method  = assert_raises(NoMethodError){ user.friend }
     @error_from_module_method   = assert_raises(NoMethodError){ user.fr0m_module }
     @error_from_class_method    = assert_raises(NoMethodError){ User.l0ad }
+    @error_from_similar_class_method  = assert_raises(NoMethodError){ Users.last_name }
+    @error_from_similar_module_method  = assert_raises(NoMethodError){ Users.pass }
   end
 
   def test_similar_words
@@ -33,6 +18,8 @@ class SimilarMethodFinderTest < Minitest::Test
     assert_suggestion @error_from_private_method.suggestions,  "friends"
     assert_suggestion @error_from_module_method.suggestions,   "from_module"
     assert_suggestion @error_from_class_method.suggestions,    "load"
+    assert_suggestion @error_from_similar_class_method.suggestions,    %w{last_names User.last_name}
+    assert_suggestion @error_from_similar_module_method.suggestions,    %w{hash class User.pass User.hash User.class}
   end
 
   def test_did_you_mean?
@@ -40,10 +27,44 @@ class SimilarMethodFinderTest < Minitest::Test
     assert_match "Did you mean? #friends",     @error_from_private_method.did_you_mean?
     assert_match "Did you mean? #from_module", @error_from_module_method.did_you_mean?
     assert_match "Did you mean? .load",        @error_from_class_method.did_you_mean?
+    assert_match "Did you mean? .last_names\n                  User.last_name",        @error_from_similar_class_method.did_you_mean?
+    assert_match ["Did you mean? .hash", ".class", "User.pass", "User.hash", "User.class" ].join("\n                  "),        @error_from_similar_module_method.did_you_mean?
   end
 
   def test_similar_words_for_long_method_name
     error = assert_raises(NoMethodError){ User.new.dependents }
     assert_suggestion error.suggestions, "descendants"
+  end
+end
+
+class User
+  def friends; end
+  def first_name; end
+  def descendants; end
+
+  class << self
+    def last_name; end
+  end
+
+  class << self
+    def pass; end
+  end
+
+  private
+
+  def friend; end
+
+  class << self
+    def load; end
+  end
+end
+
+module UserModule
+  def from_module; end
+end
+
+module Users
+  class << self
+    def last_names; end
   end
 end

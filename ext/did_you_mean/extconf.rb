34388f0a7a9fc238a3b169683438169047fa0216
Yuki Nishijima <mail@yukinishijima.net>
2015-04-01 18:05:56 -0700

    Do not compile extensions on JRuby and Rubinius
    Also, put the .jar file in the gem. Compiled Java code is portable to
    any JVM, so JRuby extensions shouldn't be compiled on site.
    
    closes #47

--- ext/did_you_mean/extconf.rb
+++ ext/did_you_mean/extconf.rb
@@ -1,3 +1,7 @@
-require 'mkmf'
-
-create_makefile 'did_you_mean/method_receiver'
+case RUBY_ENGINE
+when 'ruby'
+  require 'mkmf'
+  create_makefile 'did_you_mean/method_receiver'
+else
+  File.open("Makefile", "w") {|file| file.write "install:\n\t:\n" }
+end

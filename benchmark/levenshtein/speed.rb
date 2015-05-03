63a8b8e6f0538877ed2fe012a4b161b1e08d85ef
Yuki Nishijima <mail@yukinishijima.net>
2015-05-03 11:48:59 -0700

    Speed up Levenshtein distance calculation by about 40%
      Calculating -------------------------------------
                enumerable     1.304k i/100ms
                     while     1.791k i/100ms
      -------------------------------------------------
                enumerable     14.215k (± 6.7%) i/s -     71.720
                     while     20.479k (± 2.5%) i/s -    103.878
    
      Comparison:
                     while:    20478.7 i/s
                enumerable:    14214.6 i/s - 1.44x slower

--- benchmark/levenshtein/speed.rb
+++ benchmark/levenshtein/speed.rb
@@ -1,13 +1,16 @@
 require 'benchmark/ips'
 require 'did_you_mean'
+require 'did_you_mean/levenshtein'
+
+STR1, STR2 = "user_signed_in?", "user_logged_in?"
 
 Benchmark.ips do |x|
-  x.report "each_char" do
-    DidYouMean::Levenshtein.before_distance "user_signed_in?", "user_logged_in?"
+  x.report "enumerable" do
+    DidYouMean::Levenshtein.before_distance STR1, STR2
   end
 
-  x.report "each_codepoint" do
-    DidYouMean::Levenshtein.after_distance "user_signed_in?", "user_logged_in?"
+  x.report "while" do
+    DidYouMean::Levenshtein.after_distance STR1, STR2
   end
 
   x.compare!

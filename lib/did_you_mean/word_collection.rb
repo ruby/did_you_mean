64ed053a3edc394a0e2d5b2f1f872dfbf77be609
Yuki Nishijima <mail@yukinishijima.net>
2015-05-03 19:58:03 -0700

    Do not provide too many suggestions

--- lib/did_you_mean/word_collection.rb
+++ lib/did_you_mean/word_collection.rb
@@ -1,4 +1,5 @@
 require "delegate"
+require "did_you_mean/levenshtein"
 require "did_you_mean/jaro_winkler"
 
 module DidYouMean
@@ -14,13 +15,11 @@ module DidYouMean
 
     def similar_to(target_word)
       target_word = MemoizingString.new(target_word.to_s.downcase)
-      threshold   = target_word.length > 3 ? 0.831 : 0.77
 
-      map {|word| [JaroWinkler.distance(word.to_s.downcase, target_word), word] }
-        .select {|distance, _| distance >= threshold }
-        .sort
-        .reverse
-        .map(&:last)
+      map {|word| Pair.new(word, target_word) }
+        .select(&:close_enough?)
+        .sort {|a,b| a.levenshtein <=> b.levenshtein }
+        .map(&:word)
     end
 
     class MemoizingString < SimpleDelegator #:nodoc:
@@ -28,6 +27,34 @@ module DidYouMean
       def codepoints; @codepoints ||= super; end
     end
 
-    private_constant :MemoizingString
+    class Pair #:nodoc:
+      attr_reader :word, :input
+
+      def initialize(word, input)
+        @word  = word
+        @input = input
+      end
+
+      def jaro_winkler
+        @jaro_winkler ||= JaroWinkler.distance(word.to_s.downcase, input)
+      end
+
+      def levenshtein
+        @levenshtein ||= Levenshtein.distance(word.to_s.downcase, input)
+      end
+
+      def close_enough?
+        jaro_winkler >= (input.length > 3 ? 0.834 : 0.77) && (levenshtein <= 1 || jaro_winkler >= threshold)
+      end
+
+      private
+
+      def threshold
+        num = (- 0.6 / levenshtein) + 1
+        num + ((1 - num) * ((levenshtein.to_f / input.length) ** Math::E))
+      end
+    end
+
+    private_constant :MemoizingString, :Pair
   end
 end
--- test/word_collection_test.rb
+++ test/word_collection_test.rb
@@ -3,13 +3,40 @@ require_relative 'test_helper'
 class WordCollectionTest < Minitest::Test
 
   def test_similar_to
-    assert_suggestion 'foo',         collection('foo',                   similar_to: 'doo')
-    assert_suggestion 'drag_to',     collection('drag_to',               similar_to: 'drag')
-    assert_suggestion 'descendants', collection('descendants',           similar_to: 'dependents')
-    assert_suggestion 'email',       collection('email', 'fail', 'eval', similar_to: 'meail')
-    assert_suggestion 'email',       collection('email', 'fail', 'eval', similar_to: 'email')
+    assert_suggestion 'foo',               collection('foo', 'fork')          .similar_to('doo')
+    assert_suggestion 'drag_to',           collection('drag_to')              .similar_to('drag')
+    assert_suggestion 'descendants',       collection('descendants')          .similar_to('dependents')
+    assert_suggestion 'email',             collection('email', 'fail', 'eval').similar_to('meail')
+    assert_suggestion 'fail',              collection('email', 'fail', 'eval').similar_to('fial')
+    assert_suggestion 'fail',              collection('email', 'fail', 'eval').similar_to('afil')
+    assert_suggestion 'eval',              collection('email', 'fail', 'eval').similar_to('eavl')
+    assert_suggestion 'eval',              collection('email', 'fail', 'eval').similar_to('veal')
+    assert_suggestion 'sub!',              collection('sub', 'gsub', 'sub!')  .similar_to('suv!')
+    assert_suggestion 'set_result_count',  collection('set_result_count')     .similar_to('set_result')
 
-    assert_equal ['sub', 'sub!'], collection('sub', 'gsub', 'sub!', similar_to: 'suv')
+    assert_equal %w(sub),        collection('sub', 'gsub', 'sub!') .similar_to('suv')
+    assert_equal %w(gsub! gsub), collection('sub', 'gsub', 'gsub!').similar_to('gsuv!')
+
+    group_methods = %w(groups group_url groups_url group_path)
+    assert_suggestion 'groups', collection(group_methods).similar_to('group')
+
+    group_classes = %w(
+      GroupMembership
+      GroupMembershipPolicy
+      GroupMembershipDecorator
+      GroupMembershipSerializer
+      GroupHelper
+      GroupMembershipWithCachedEmailDecorator
+      Group
+      GroupMailer
+      NullGroupMembership
+    )
+
+    #assert_suggestion 'GroupMembership',          collection(group_classes).similar_to('GroupMemberhip')
+    assert_suggestion 'GroupMembershipDecorator', collection(group_classes).similar_to('GroupMemberhipDecorator')
+
+    assert_empty collection('proc').similar_to 'product_path'
+    assert_empty collection('fork').similar_to 'fooo'
   end
 
   def test_similar_to_sorts_results_by_simiarity
@@ -35,11 +62,6 @@ class WordCollectionTest < Minitest::Test
   private
 
   def collection(*args)
-    if args.last.is_a?(Hash)
-      options = args.pop
-      DidYouMean::WordCollection.new(args).similar_to(options[:similar_to])
-    else
-      DidYouMean::WordCollection.new(args)
-    end
+    DidYouMean::WordCollection.new(args.flatten)
   end
 end

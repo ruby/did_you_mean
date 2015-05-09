d58d8f27f3c8729fab2e450d9c8603ee84229a6e
Yuki Nishijima <mail@yukinishijima.net>
2015-05-09 12:44:58 -0700

    Set the binding object to the exception if it's in the finder list

--- lib/did_you_mean.rb
+++ lib/did_you_mean.rb
@@ -10,7 +10,7 @@ module DidYouMean
     # On IRB/pry console, this event is called twice. In the second event,
     # we get IRB/pry binding. So it shouldn't override @frame_binding if
     # it's already defined.
-    if exception.is_a?(NameError) && !exception.instance_variable_defined?(:@frame_binding)
+    if DidYouMean.finders.include?(exception.class.to_s) && !exception.instance_variable_defined?(:@frame_binding)
       exception.instance_variable_set(:@frame_binding, binding)
     end
   end

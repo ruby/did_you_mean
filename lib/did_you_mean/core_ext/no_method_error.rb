ab8d1438ea3cef0139f09e2db8d340ce1097a48f
Yuki Nishijima <mail@yukinishijima.net>
2015-05-10 16:37:57 -0700

    Use use JRuby.reference(obj) and Java's reflection even on JRuby 1.7.x

--- lib/did_you_mean/core_ext/no_method_error.rb
+++ lib/did_you_mean/core_ext/no_method_error.rb
@@ -4,31 +4,32 @@ when 'ruby'
 
 when 'jruby'
   NoMethodError.class_eval do
-    if JRUBY_VERSION >= '9.0.0.0'
-      def to_s
-        receiver unless defined?(@receiver)
+    def to_s
+      receiver unless defined?(@receiver)
+      super
+    end
+
+    def receiver
+      @receiver ||= begin
+        field = JRuby.reference(__message__).java_class.getDeclaredField("object")
+        field.setAccessible(true)
+        field.get(__message__)
+      rescue
         super
       end
+    end
 
-      def receiver
-        @receiver ||= begin
-          field = JRuby.reference(__message__).java_class.getDeclaredField("object")
-          field.setAccessible(true)
-          field.get(__message__)
-        rescue
-          nil
-        end
-      end
+    private
 
-      private
-
+    if JRUBY_VERSION >= '9.0.0.0'
       def __message__
         JRuby.reference(self).getMessage
       end
     else
-      require 'did_you_mean/receiver_capturer'
-      org.yukinishijima.ReceiverCapturer.setup(JRuby.runtime)
-      attr_reader :receiver
+      def __message__
+        error = JRuby.reference(self)
+        error.java_class.getField("message").get(error)
+      end
     end
   end
 end

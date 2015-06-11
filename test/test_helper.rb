ecf59b3b6502cbf1c913f7573bad5430af7b4f6d
Yuki Nishijima <mail@yukinishijima.net>
2015-06-11 04:21:59 -0700

    No need to fall back to  MiniTest::Unit::TestCase any more

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,12 +4,6 @@ require 'minitest/autorun'
 require 'minitest/unit'
 require 'did_you_mean'
 
-begin
-  MiniTest::Test
-rescue NameError
-  MiniTest::Test = MiniTest::Unit::TestCase
-end
-
 module DidYouMean::TestHelper
   def assert_suggestion(expected, array)
     assert_equal [expected], array, "Expected #{array.inspect} to only include #{expected.inspect}"

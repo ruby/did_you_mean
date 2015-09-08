3ccaf10e91da08243277c1e9fd5375a3eefba976
Yuki Nishijima <mail@yukinishijima.net>
2015-09-08 04:47:26 -0700

    Refactor the DidYouMean::Formatter class

--- lib/did_you_mean/formatter.rb
+++ lib/did_you_mean/formatter.rb
@@ -7,13 +7,8 @@ module DidYouMean
     def to_s
       return "".freeze if @corrections.empty?
 
-      output = "\n"
-      output << "Did you mean?  #{format(@corrections.first)}"
-      output << @corrections.drop(1).map{|word| "\n#{' '.freeze * 15}#{format(word)}" }.join
-    end
-
-    def format(name)
-      name
+      output = "\nDid you mean?  "
+      output << @corrections.join("\n" << ' '.freeze * 15)
     end
   end
 end

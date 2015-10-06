7a8b68ca9fa89f6f5130097dc036c5b0b22a8724
Yuki Nishijima <mail@yukinishijima.net>
2015-10-06 04:03:49 -0700

    Refactor SpellCheckerTest
    Maybe this is more readable...?

--- test/spell_checker_test.rb
+++ test/spell_checker_test.rb
@@ -1,40 +1,29 @@
 require 'test_helper'
 
 class SpellCheckerTest < Minitest::Test
-  class WordCollection
+  SpellChecker = Struct.new(:input, :words) do
     include DidYouMean::SpellCheckable
 
-    def initialize(words)
-      @words = words
-    end
-
-    def similar_to(input)
-      @corrections, @input = nil, input
-
-      corrections
-    end
-
     def candidates
-      { @input => @words }
+      { input => words }
     end
   end
 
   def test_similar_to_corrects_mistypes
+    assert_spell 'foo',   input: 'doo',   dictionary: ['foo', 'fork']
+    assert_spell 'email', input: 'meail', dictionary: ['email', 'fail', 'eval']
+    assert_spell 'fail',  input: 'fial',  dictionary: ['email', 'fail', 'eval']
+    assert_spell 'fail',  input: 'afil',  dictionary: ['email', 'fail', 'eval']
+    assert_spell 'eval',  input: 'eavl',  dictionary: ['email', 'fail', 'eval']
+    assert_spell 'eval',  input: 'veal',  dictionary: ['email', 'fail', 'eval']
+    assert_spell 'sub!',  input: 'suv!',  dictionary: ['sub', 'gsub', 'sub!']
+    assert_spell 'sub',   input: 'suv',   dictionary: ['sub', 'gsub', 'sub!']
 
-    assert_correction 'foo',         collection('foo', 'fork')          .similar_to('doo')
-    assert_correction 'email',       collection('email', 'fail', 'eval').similar_to('meail')
-    assert_correction 'fail',        collection('email', 'fail', 'eval').similar_to('fial')
-    assert_correction 'fail',        collection('email', 'fail', 'eval').similar_to('afil')
-    assert_correction 'eval',        collection('email', 'fail', 'eval').similar_to('eavl')
-    assert_correction 'eval',        collection('email', 'fail', 'eval').similar_to('veal')
-    assert_correction 'sub!',        collection('sub', 'gsub', 'sub!')  .similar_to('suv!')
-    assert_correction 'sub',         collection('sub', 'gsub', 'sub!')  .similar_to('suv')
-
-    assert_equal %w(gsub! gsub),     collection('sub', 'gsub', 'gsub!').similar_to('gsuv!')
-    assert_equal %w(sub! sub gsub!), collection('sub', 'sub!', 'gsub', 'gsub!').similar_to('ssub!')
+    assert_equal %w(gsub! gsub),     SpellChecker.new('gsuv!', %w(sub gsub gsub!)).corrections
+    assert_equal %w(sub! sub gsub!), SpellChecker.new('ssub!', %w(sub sub! gsub gsub!)).corrections
 
     group_methods = %w(groups group_url groups_url group_path)
-    assert_correction 'groups', collection(group_methods).similar_to('group')
+    assert_spell 'groups', input: 'group',  dictionary: group_methods
 
     group_classes = %w(
       GroupMembership
@@ -47,20 +36,20 @@ class SpellCheckerTest < Minitest::Test
       NullGroupMembership
     )
 
-    assert_correction 'GroupMembership',          collection(group_classes).similar_to('GroupMemberhip')
-    assert_correction 'GroupMembershipDecorator', collection(group_classes).similar_to('GroupMemberhipDecorator')
+    assert_spell 'GroupMembership',          dictionary: group_classes, input: 'GroupMemberhip'
+    assert_spell 'GroupMembershipDecorator', dictionary: group_classes, input: 'GroupMemberhipDecorator'
 
     names = %w(first_name_change first_name_changed? first_name_will_change!)
-    assert_equal names, collection(names).similar_to('first_name_change!')
+    assert_equal names, SpellChecker.new('first_name_change!', names).corrections
 
-    assert_empty collection('proc').similar_to 'product_path'
-    assert_empty collection('fork').similar_to 'fooo'
+    assert_empty SpellChecker.new('product_path', ['proc']).corrections
+    assert_empty SpellChecker.new('fooo',         ['fork']).corrections
   end
 
   def test_similar_to_corrects_misspells
-    assert_correction 'descendants',      collection('descendants')     .similar_to('dependents')
-    assert_correction 'drag_to',          collection('drag_to')         .similar_to('drag')
-    assert_correction 'set_result_count', collection('set_result_count').similar_to('set_result')
+    assert_spell 'descendants',      input: 'dependents', dictionary: ['descendants']
+    assert_spell 'drag_to',          input: 'drag',       dictionary: ['drag_to']
+    assert_spell 'set_result_count', input: 'set_result', dictionary: ['set_result_count']
   end
 
   def test_similar_to_sorts_results_by_simiarity
@@ -71,20 +60,21 @@ class SpellCheckerTest < Minitest::Test
       name123
     )
 
-    actual = WordCollection.new(%w(
+    actual = SpellChecker.new("name123456", %w(
       name12
       name123
       name1234
       name12345
       name123456
-    )).similar_to("name123456")
+    )).corrections
 
     assert_equal expected, actual
   end
 
   private
 
-  def collection(*args)
-    WordCollection.new(args.flatten)
+  def assert_spell(expected, input: , dictionary: )
+    corrections = SpellChecker.new(input, dictionary).corrections
+    assert_equal [expected], corrections, "Expected to suggest #{expected}, but got #{corrections.inspect}"
   end
 end

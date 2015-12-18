8a0abc93a25eb92f541c3d47b18c75dd2e959459
Yuki Nishijima <mail@yukinishijima.net>
2015-12-18 05:41:04 -0800

    Re-organize code

--- lib/did_you_mean.rb
+++ lib/did_you_mean.rb
@@ -9,14 +9,6 @@ require 'did_you_mean/spell_checkers/null_checker'
 require "did_you_mean/formatter"
 
 module DidYouMean
-  TRACE = TracePoint.new(:raise) do |tp|
-    e = tp.raised_exception
-
-    if SPELL_CHECKERS.include?(e.class.to_s) && !e.instance_variable_defined?(:@frame_binding)
-      e.instance_variable_set(:@frame_binding, tp.binding)
-    end
-  end
-
   IGNORED_CALLERS = []
 
   SPELL_CHECKERS = Hash.new(NullChecker)
--- lib/did_you_mean/extra_features.rb
+++ lib/did_you_mean/extra_features.rb
@@ -1,4 +1,15 @@
+require 'did_you_mean'
+
 module DidYouMean
-  TRACE.enable
-  SPELL_CHECKERS['NoMethodError'].prepend(MethodNameChecker::IvarNameCorrectable)
+  TRACE = TracePoint.trace(:raise) do |tp|
+    e = tp.raised_exception
+
+    if SPELL_CHECKERS.include?(e.class.to_s) && !e.instance_variable_defined?(:@frame_binding)
+      e.instance_variable_set(:@frame_binding, tp.binding)
+    end
+  end
+
+  NameError.send(:attr, :frame_binding)
 end
+
+require 'did_you_mean/extra_features/ivar_name_correction'

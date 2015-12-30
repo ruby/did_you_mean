3b2597afdd19622e281af0431a437c3dc06bcbdb
Mitsuteru Sawa <mitsuteru.s@gmail.com>
2015-12-31 01:29:50 +0900

    rubocop: Use lambda
    Method definitions must not be nested. Use lambda instead.

--- test/extra_features/initializer_name_correction_test.rb
+++ test/extra_features/initializer_name_correction_test.rb
@@ -3,13 +3,13 @@ require 'test_helper'
 class InitializerNameCorrectionTest < Minitest::Test
   def test_corrects_wrong_initializer_name
     assert_output nil, "warning: intialize might be misspelled, perhaps you meant initialize?\n" do
-      Class.new { def intialize; end }
+      Class.new -> { intialize }
     end
   end
 
   def test_does_not_correct_correct_initializer_name
     assert_output nil, "" do
-      Class.new { def initialize; end }
+      Class.new -> { initialize }
     end
   end
 end

00e305971060d150fae4817b5e895d6478b37579
Yuki Nishijima <mail@yukinishijima.net>
2016-05-09 22:27:52 +0000

    Call a method in VerboseFormatterTest
    The local variable name correction is disabled on JRuby, so other type correction should be made in this test.

--- test/verbose_formatter_test.rb
+++ test/verbose_formatter_test.rb
@@ -2,15 +2,14 @@ require 'test_helper'
 
 class VerboseFormatterTest < Minitest::Test
   def setup
-    does_exist = does_exist = nil
-    @error = assert_raises(NameError){ doesnt_exist }
+    @error = assert_raises(NameError){ self.inspectt }
   end
 
   def test_message
     assert_equal <<~MESSAGE.chomp, @error.message
-      undefined local variable or method `doesnt_exist' for #{method(:to_s).super_method.call}
+      undefined method `inspectt' for #{method(:to_s).super_method.call}
 
-          Did you mean? does_exist
+          Did you mean? inspect
        
     MESSAGE
   end

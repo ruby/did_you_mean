6fb486e2dae2789ebeac81607ec0fd996ef4b9d9
Yuki Nishijima <mail@yukinishijima.net>
2016-12-10 22:56:08 +0000

    Print out the version of did you mean to test against

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,8 @@ require 'minitest/unit'
 require 'purdytest'
 require 'did_you_mean'
 
+puts "DidYouMean version: #{DidYouMean::VERSION}"
+
 module DidYouMean::TestHelper
   def assert_correction(expected, array)
     assert_equal Array(expected), array, "Expected #{array.inspect} to only include #{expected.inspect}"

cd8722d00574c33c4f5894ced70b88de498f5120
Yuki Nishijima <yk.nishijima@gmail.com>
2017-07-05 12:55:52 -0400

    Add copyright notice

--- test/edit_distance/jaro_winkler_test.rb
+++ test/edit_distance/jaro_winkler_test.rb
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 require 'test_helper'
 
+# These tests were originally written by Jian Weihang (簡煒航) as part of his work
+# on the jaro_winkler gem. The original code could be found here:
+#   https://github.com/tonytonyjan/jaro_winkler/blob/9bd12421/spec/jaro_winkler_spec.rb
+#
+# Copyright (c) 2014 Jian Weihang
+
 class JaroWinklerTest < Minitest::Test
   def test_jaro_winkler_distance
     assert_distance 0.9667, 'henka',      'henkan'

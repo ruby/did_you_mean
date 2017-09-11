ab94ada6b712f3205333d5d9165e67e2a9583329
Yuki Nishijima <yk.nishijima@gmail.com>
2017-09-11 15:23:56 -0400

    WIP

--- lib/did_you_mean/static.rb
+++ lib/did_you_mean/static.rb
@@ -8,21 +8,29 @@ PAYLOAD = {
   did_you_mean_version: DidYouMean::VERSION
 }
 
+current_dir = Dir.pwd
+
 at_exit {
-  detector = UndefinedMethodDetector.new(__dir__)
+  detector = UndefinedMethodDetector.new(current_dir)
 
   PAYLOAD[:undefined_names] = detector
     .undefined_methods
-    .flat_map do |undefined_method|
+    .flat_map { |undefined_method|
       undefined_method.called_by.map do |method_calling_undefined_method|
+        path = method_calling_undefined_method.source_location[0]
+        path = File.join(current_dir, path) if !path.start_with?(current_dir)
+
         {
           undefined_name: undefined_method.name,
           symbol_type: "method",
-          path: "#{__dir__}/#{method_calling_undefined_method.source_location[0]}",
+          path: path,
           lineno: undefined_method.lineno,
+          "suggestions": DidYouMean::SpellChecker.new(dictionary: detector.all_defined_method_names).correct(undefined_method.name).uniq
         }
       end
-    end
+    }
+    .uniq
+    .group_by {|method_info| method_info[:lineno] }
 
   puts JSON.pretty_generate(PAYLOAD)
 }

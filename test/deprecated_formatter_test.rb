5c85439490369f6004961d2742bbad979bf7f04b
Yuki Nishijima <yk.nishijima@gmail.com>
2018-04-03 00:15:34 -0400

    Regex match over exact match for the test for the deprecated formatter

--- test/deprecated_formatter_test.rb
+++ test/deprecated_formatter_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class DeprecatedFormatterTest < Minitest::Test
   def test_deprecated_formatter
-    assert_output nil, "test/deprecated_formatter_test.rb:6: warning: constant DidYouMean::Formatter is deprecated\n" do
+    assert_output nil, /test\/deprecated_formatter_test.rb:6: warning: constant DidYouMean::Formatter is deprecated/ do
       assert_equal "\nDid you mean?  does_exist", ::DidYouMean::Formatter.new(['does_exist']).to_s
     end
   end

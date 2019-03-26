b26ed34be1a9eb06f213eace17d613332708886d
Chris Drane <obromois@gmail.com>
2019-03-26 21:02:52 +1100

    Working on print out

--- test/tree_spell_checker_test.rb
+++ test/tree_spell_checker_test.rb
@@ -21,18 +21,35 @@ class TreeSpellCheckerTest  < Minitest::Test
   end
 
   def test_human_typo_exercise
+    n_repeat = 1000
     first_times = [0, 0]
     total_suggestions = [0, 0]
-    files = Dir["test/**/*.rb"]
+    total_failures = [0, 0]
+    files = Dir['test/**/*.rb']
     len = files.length
-    100.times do
+    n_repeat.times do
       word = files[rand len]
       word_error = HumanTypo.new(word).call
       suggestions_a = group_suggestions word_error, files
-      check_first_is_right(word, suggestions_a, first_times, word_error)
-      check_no_suggestions(suggestions_a, total_suggestions)
+      check_first_is_right word, suggestions_a, first_times
+      check_no_suggestions suggestions_a, total_suggestions
+      check_for_failure word, suggestions_a, total_failures
     end
-    pp "first_times #{first_times}, totals #{total_suggestions}"
+    print_results first_times, total_suggestions, total_failures, n_repeat
+  end
+
+  def print_results(first_times, total_suggestions, total_failures, n_repeat)
+   algorithms = %w(Tree DYM_ Comb)
+    pp "                         Summary "
+    pp "----------------------------------------------------------------------------------"
+    pp " Method  |   First Time (\%)    Mean Suggestions       Failures (\%)"
+    pp "----------------------------------------------------------------------------------"
+    (0..1).each do |i|
+      ft = (first_times[i].to_f / n_repeat * 100).round(1)
+      mns = (total_suggestions[i].to_f / (n_repeat)).round(2)
+      f = (total_failures[i].to_f / n_repeat * 100).round(1)
+      pp " #{algorithms[i]}          #{ft}                  #{mns}                 #{f}"
+    end
   end
 
   def group_suggestions(word_error, files)
@@ -41,12 +58,15 @@ class TreeSpellCheckerTest  < Minitest::Test
     [a0, a1]
   end
 
-  def check_first_is_right(word, suggestions_a, first_times, word_error = nil)
+  def check_for_failure(word, suggestions_a, total_failures)
     suggestions_a.each_with_index.map do |a, i|
-      if word == a.first
-        first_times[i] += 1
-      else
-      end
+      total_failures[i] += 1 unless a.include? word
+    end
+  end
+
+  def check_first_is_right(word, suggestions_a, first_times)
+    suggestions_a.each_with_index.map do |a, i|
+      first_times[i] += 1 if word == a.first
     end
   end
 

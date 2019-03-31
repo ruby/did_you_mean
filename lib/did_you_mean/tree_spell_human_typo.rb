4c4dba0fd7421d9b2bc0784ad404ebab80e6e2be
Chris Drane <obromois@gmail.com>
2019-03-31 17:34:28 +1100

    Refactor HumanTypo

--- lib/did_you_mean/tree_spell_human_typo.rb
+++ lib/did_you_mean/tree_spell_human_typo.rb
@@ -1,25 +1,21 @@
+# Simulate a error prone human typist
+# Assumes typographical errors are Poisson distributed and
+# each error is either a deletion, insertion, or transposition
 class TreeSpellHumanTypo
-
-  LAMBDA = 0.05
+  LAMBDA = 0.05 # The typographical error rate of the Poisson distribution
 
   def initialize(input)
-    @word = input.dup
-    check_word
-    @len = word.length
+    @input = input
+    check_input
+    @len = input.length
   end
 
   def call
+    @word = input.dup
     i_place = initialize_i_place
     loop do
       action = action_type
-      case action
-      when :delete
-        @word = deletion(i_place)
-      when :insert
-        @word = insertion(i_place, rand_char)
-      when :transpose
-        @word = transposition(i_place, toss)
-      end
+      @word = make_change action, i_place
       @len = word.length
       i_place += exponential
       break if i_place >= len
@@ -27,9 +23,24 @@ class TreeSpellHumanTypo
     word
   end
 
+  def insertion_test(i_place, char)
+    @word = input.dup
+    insertion(i_place, char)
+  end
+
+  def deletion_test(i_place)
+    @word = input.dup
+    deletion(i_place)
+  end
+
+  def transposition_test(i_place, direction)
+    @word = input.dup
+    transposition(i_place, direction)
+  end
+
   private
 
-  attr_accessor :word, :len
+  attr_accessor :input, :word, :len
 
   def initialize_i_place
     i_place = nil
@@ -45,9 +56,17 @@ class TreeSpellHumanTypo
   end
 
   def rand_char
-    popular_chars = ('a'..'z').to_a.join + ('A'..'Z').to_a.join + '?<>,.!`+=-_":;@#$%^&*()'
+    popular_chars =  alphabetic_characters + special_characters
     n = popular_chars.length
     popular_chars[rand(n)]
+  end
+
+  def alphabetic_characters
+    ('a'..'z').to_a.join + ('A'..'Z').to_a.join
+  end
+
+  def special_characters
+    '?<>,.!`+=-_":;@#$%^&*()'
   end
 
   def toss
@@ -59,31 +78,57 @@ class TreeSpellHumanTypo
     [:insert, :transpose, :delete][rand(3)]
   end
 
+  def make_change(action, i_place)
+    case action
+    when :delete
+      deletion(i_place)
+    when :insert
+      insertion(i_place, rand_char)
+    when :transpose
+      transposition(i_place, toss)
+    end
+  end
+
   # insert char after index of i_place
   def insertion(i_place, char)
     return char + word if i_place == 0
     return word + char if i_place == len - 1
-    word[0..i_place] + char + word[(i_place + 1)..-1]
+    word.insert(i_place + 1, char)
   end
 
   def deletion(i_place)
-    return word[0..-2] if i_place == len
-    return word[1..-1] if i_place.zero?
-    word[0...i_place] + word[(i_place + 1)..-1]
+    word.slice!(i_place)
+    word
   end
 
   # transpose char at i_place with char at i_place + direction
   # if i_place + direction is out of bounds just swap in other direction
   def transposition(i_place, direction)
     w = word.dup
-    return w[1] + w[0] + word[2..-1] if i_place + direction < 0
-    return w[0...(len - 2)] + word[len - 1] + word[len - 2] if i_place + direction >= len
-    w[i_place] = word[i_place + direction]
-    w[i_place + direction] = word[i_place]
+    return  swap_first_two(w) if i_place + direction < 0
+    return  swap_last_two(w) if i_place + direction >= len
+    swap_two(w, i_place, direction)
     w
   end
 
-  def check_word
-    fail "Word length must be greater than 5 characters: #{word}" if word.nil? || word.length < 5
+  def swap_first_two(w)
+    w[1] + w[0] + word[2..-1]
+  end
+
+  def swap_last_two(w)
+    w[0...(len - 2)] + word[len - 1] + word[len - 2]
+  end
+
+  def swap_two(w, i_place, direction)
+    w[i_place] = word[i_place + direction]
+    w[i_place + direction] = word[i_place]
+  end
+
+  def check_input
+    fail check_input_message if input.nil? || input.length < 5
+  end
+
+  def check_input_message
+    "input length must be greater than 5 characters: #{input}"
   end
 end
--- test/tree_spell_human_typo_test.rb
+++ test/tree_spell_human_typo_test.rb
@@ -2,34 +2,34 @@ require 'test_helper'
 
 class TreeSpellHumanTypoTest < Minitest::Test
   def setup
-    @word = 'spec/services/anything_spec'
-    @sh = TreeSpellHumanTypo.new(@word)
-    @len = @word.length
+    @input = 'spec/services/anything_spec'
+    @sh = TreeSpellHumanTypo.new(@input)
+    @len = @input.length
   end
 
   def test_deleletion
-    assert_match @sh.send(:deletion, 5), 'spec/ervices/anything_spec'
-    assert_match @sh.send(:deletion, @len), 'spec/services/anything_spe'
-    assert_match @sh.send(:deletion, 0), 'pec/services/anything_spec'
+    assert_match @sh.deletion_test(5), 'spec/ervices/anything_spec'
+    assert_match @sh.deletion_test(@len - 1), 'spec/services/anything_spe'
+    assert_match @sh.deletion_test(0), 'pec/services/anything_spec'
   end
 
   def test_insertion
-    assert_match @sh.send(:insertion, 7, 'X'), 'spec/serXvices/anything_spec'
-    assert_match @sh.send(:insertion, 0, 'X'), 'Xspec/services/anything_spec'
-    assert_match @sh.send(:insertion, @len - 1, 'X'), 'spec/services/anything_specX'
+    assert_match @sh.insertion_test(7, 'X'), 'spec/serXvices/anything_spec'
+    assert_match @sh.insertion_test(0, 'X'), 'Xspec/services/anything_spec'
+    assert_match @sh.insertion_test(@len - 1, 'X'), 'spec/services/anything_specX'
   end
 
   def test_transposition
-    n = @word.length
-    assert_match @sh.send(:transposition, 0, -1), 'psec/services/anything_spec'
-    assert_match @sh.send(:transposition, n - 1, +1), 'spec/services/anything_spce'
-    assert_match @sh.send(:transposition, 4, +1), 'specs/ervices/anything_spec'
-    assert_match @sh.send(:transposition, 4, -1), 'spe/cservices/anything_spec'
-    assert_match @sh.send(:transposition, 21, -1), 'spec/services/anythign_spec'
-    assert_match @sh.send(:transposition, 21, +1), 'spec/services/anythin_gspec'
+    n = @input.length
+    assert_match @sh.transposition_test(0, -1), 'psec/services/anything_spec'
+    assert_match @sh.transposition_test(n - 1, +1), 'spec/services/anything_spce'
+    assert_match @sh.transposition_test(4, +1), 'specs/ervices/anything_spec'
+    assert_match @sh.transposition_test(4, -1), 'spe/cservices/anything_spec'
+    assert_match @sh.transposition_test(21, -1), 'spec/services/anythign_spec'
+    assert_match @sh.transposition_test(21, +1), 'spec/services/anythin_gspec'
   end
 
   def test_for_change
-    refute_match @sh.call, @word
+    refute_match @sh.call, @input
   end
 end
